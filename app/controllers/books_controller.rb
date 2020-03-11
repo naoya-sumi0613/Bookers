562c0b561c14e31efeda9f70440567a0334ae8f4
naoya-sumi0613 <s.naoya613pcstudy@gmail.com>
2020-03-11 17:55:59 +0900

    [add]4-8 動作完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,41 @@
 class BooksController < ApplicationController
   def index
   	  @books = Book.all
-  	  @book = Book.new
+      @book = Book.new
   end
 
   def show
+      @book = Book.find(params[:id])
   end
 
   def edit
+      @book = Book.find(params[:id])
+  end
+
+  def update
+      @book = Book.find(params[:id])
+      if @book.update(book_params)
+         redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
+      else
+         render action: :edit
+      end
   end
 
   def create
-      book = Book.new(book_params)
-      book.save
-      redirect_to books_path
+      @book = Book.new(book_params)
+      if @book.save
+         redirect_to book_path(book.id), notice: 'Book was successfully created.'
+      else
+         @books = Book.all
+         render action: :index
+      end
   end
 
+  def destroy
+      book = Book.find(params[:id])
+      book.destroy
+      redirect_to books_path, notice: 'Book was successfully destroyed.'
+  end
 
   private
   def book_params
